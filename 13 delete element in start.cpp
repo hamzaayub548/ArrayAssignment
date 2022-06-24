2da4a0d0f859037d28f9020762bc29b546dc88dd
Hamza <107944804+hamzaayub548@users.noreply.github.com>
2022-06-24 16:54:28 +0500

    Add files via upload

---
+++ 13 delete element in start.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Array{
+	private:
+		int arr[10];
+	public:
+	void input(){
+		cout<<"Enter Ten Elements of Array:"<<endl;
+		for(int i=0;i<10;i++){
+			cin>>arr[i];
+		}
+	}
+	void removing(){
+		cout<<"After Removing Element From Starting Position: ";
+		for(int i=0;i<10;i++){
+			arr[i]=arr[i+1];
+		}
+		for(int i=0;i<9;i++){
+			cout<<arr[i];
+		}
+	}	
+};
+int main(){
+	Array obj;
+	obj.input();
+	obj.removing();
+	return 0;
+}
